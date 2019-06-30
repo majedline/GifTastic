ebe221058449871d8778846b753bf6efd301d1db
majedline <majed.atwi@gmail.com>
2019-06-30 14:44:31 -0400

    added button builder, button remove, and updated html to remove static buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,12 +20,41 @@ function showImage(response) {
     // $("#a1").html(imgTag);
 
 }
-buildGifyCard("a", "x");
-buildGifyCard("a", "x");
-buildGifyCard("a", "x");
-buildGifyCard("a", "x");
+
+buildGifyButton(1, "https://www.google.ca", "google");
+
+// function that builds the button menues. 
+// It takes and ID to define this button and control the remove event on the A, the URL to load the view-item area, and the buttonText
+function buildGifyButton(id, dataURL, buttonText) {
+    var btn = $("<button>").attr({
+        "id": "bb" + id,
+        "type": "button",
+        "class": "btn btn-outline-light single-button",
+        "data-url": dataURL
+    }).html(buttonText + " ");
+
+    btn.append(buildCloseLink(id));
+    
+    $("#menu-items").append(btn);
+}
+
+// this functions builds the X on the button with event to remove element id
+function buildCloseLink(id) {
+    // create the X hyperlink that removes the element.
+    var a = $("<a>").attr("href", "#").text("X");
+    // onclick a, remove this button
+    a.on("click", function () {
+        $("#bb" + id).remove();
+        console.log("bb" + id);
+    });
+
+    return a;
+}
 
 
+// buildGifyCard("./assets/images/sample-cat2.jpg", "xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz ");
+
+// Function that builds the gify card on the UI. This functions takes the image URL and the corresponding text
 function buildGifyCard(gifyImage, gifyText) {
 
     var viewItem = $("<div>").attr("class", "view-item");
@@ -35,34 +64,21 @@ function buildGifyCard(gifyImage, gifyText) {
     });
     var cardImg = $("<img>").attr({
         "class": "card-img-top content-image",
-        "src": "assets/images/sample-cat2.jpg",
+        "src": gifyImage,
         "alt": "Card image cap"
     });
     var cardBody = $("<div>").attr("class", "card-body");
-    var cardBodyText = $("<p>").attr("class", "card-text content-text").html("'come quick example text to build on the card title and make up the bulk'");
+    var cardBodyText = $("<p>").attr("class", "card-text content-text").html("" + gifyText + "");
 
+    // set the card text
     cardBody.append(cardBodyText);
-
-
+    // include the card image to the card
     cardItem.append(cardImg);
+    // include the card text to the card
     cardItem.append(cardBody);
-
+    // include the card to the view item
     viewItem.append(cardItem);
-
-
+    // include the view item to the main viewing area
     $("#view-area").append(viewItem);
-
-
-
-    // <div class="view-item">
-    //     <div class="card text-white bg-secondary" style="width: 18rem;">
-    //         <img class="card-img-top content-image" src="assets/images/sample-cat2.jpg" alt="Card image cap">
-    //             <div class="card-body">
-    //                 <p class="card-text content-text">Some quick example text to build on the card title and
-    //                     make up the bulk
-    //             of the card's cont.</p>
-    //             </div>
-    // </div>
-    //     </div>
 
 }
--- assets/javascript/connections.js
+++ assets/javascript/connections.js
@@ -1,6 +1,8 @@
-var key = "uOnMzSmMAIr9qzOMFZMaf9rIWawy7XDW";
+var key = "api_key=uOnMzSmMAIr9qzOMFZMaf9rIWawy7XDW";
 
-var queryURLTrending = "https://api.giphy.com/v1/gifs/trending?api_key="+key;
+var queryURLTrending = "https://api.giphy.com/v1/gifs/trending?"+key;
+
+var queryURLRandom = "api.giphy.com/v1/stickers/random?"+key;
 
 console.log("queryURLTrending: " + queryURLTrending);
 
