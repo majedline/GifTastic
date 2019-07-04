97dbd55a742b8e061d57d77cd23b8a2b14cf57c3
majedline <majed.atwi@gmail.com>
2019-07-04 00:27:19 -0400

    added code to build buttons based on the topics array. Added code to build the cards using data recieved from the webservices. Added code to card builder to include the active and static image. Added events to buttons and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,54 @@
+var topics = ["Canada", "Australia", "India", "Lebanon", "Palestine", "Russia", "Ghana", "Singapore"];
 
-
-console.log("connection to gify is ready");
-
-$.ajax({
-    url: queryURLTrending,
-    method: "GET"
-}).then(function (response) {
-    showImage(response);
-
-});
-
-function showImage(response) {
-    console.log(response);
-
-    // var imgTag = $("<img>");
-    // imgTag.attr("id", 1);
-    // imgTag.attr("src", response.url);
-    // console.log(imgTag);
-    // $("#a1").html(imgTag);
-
-}
-
-buildGifyButton(1, "https://www.google.ca", "google");
-
-// function that builds the button menues. 
+// function that builds the button menue. 
 // It takes and ID to define this button and control the remove event on the A, the URL to load the view-item area, and the buttonText
-function buildGifyButton(id, dataURL, buttonText) {
+function buildGifyButton(id, topic, buttonText) {
     var btn = $("<button>").attr({
         "id": "bb" + id,
         "type": "button",
         "class": "btn btn-outline-light single-button",
-        "data-url": dataURL
+        "data-topic": topic,
     }).html(buttonText + " ");
 
     btn.append(buildCloseLink(id));
-    
+
+    btn.on("click", buttonClickHandler);
+
     $("#menu-items").append(btn);
+}
+
+function buttonClickHandler() {
+
+    var btnURL = buildSearchURL($(this).attr("data-topic"), 5);
+    console.log(btnURL);
+
+    $.ajax({
+        url: btnURL,
+        method: "GET"
+    }).then(function (response) {
+        showImages(response);
+    });
+}
+
+function showImages(response) {
+    $("#view-area").empty();
+    console.log("showImage called. the response is:");
+    console.log(response);
+
+    var gifsToDisplay = response.data;
+
+    for (var i = 0; i < gifsToDisplay.length; i++) {
+        var txt = "<p>This GIF is rated " + gifsToDisplay[i].rating + "<p>";
+        var stillImage = gifsToDisplay[i].images.original_still.url;
+        var activeImage = gifsToDisplay[i].images.original.url;
+
+        buildGifyCard(stillImage, activeImage, txt);
+
+    }
+
+    var gtext = response.data[1].type;
+    console.log(gtext);
+
 }
 
 // this functions builds the X on the button with event to remove element id
@@ -44,29 +57,51 @@ function buildCloseLink(id) {
     var a = $("<a>").attr("href", "#").text("X");
     // onclick a, remove this button
     a.on("click", function () {
+
+        // console.log("removing bb" + id);
+        // console.log(topics);
+
         $("#bb" + id).remove();
-        console.log("bb" + id);
+
+        // topics.splice(id,1);
+        // console.log(topics);
     });
 
     return a;
 }
 
+function buildSearchURL(keyword, limit) {
+    var url = queryURLSearch;
+    url += ("&q=" + keyword);
+    url += ("&rating=g");
+    url += ("&limit=" + limit);
+    console.log(url);
 
-// buildGifyCard("./assets/images/sample-cat2.jpg", "xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz ");
+    return url;
+}
+
+// buildGifyCard("./assets/images/sample-cat2.jpg","./assets/images/sample-cat2.jpg", "xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz ");
 
 // Function that builds the gify card on the UI. This functions takes the image URL and the corresponding text
-function buildGifyCard(gifyImage, gifyText) {
+function buildGifyCard(gifyStillImage, gifyActiveImage, gifyText) {
 
     var viewItem = $("<div>").attr("class", "view-item");
     var cardItem = $("<div>").attr({
         "class": "card text-white bg-secondary",
         "style": "width: 18rem;"
     });
+    
     var cardImg = $("<img>").attr({
         "class": "card-img-top content-image",
-        "src": gifyImage,
-        "alt": "Card image cap"
+        "src": gifyStillImage,
+        "alt": "Card image cap",
+        "data-img-still": gifyStillImage,
+        "data-img-active": gifyActiveImage,
+        "data-img-state": "still"
     });
+
+    cardImg.on("click", gifyImageClickHandler);
+
     var cardBody = $("<div>").attr("class", "card-body");
     var cardBodyText = $("<p>").attr("class", "card-text content-text").html("" + gifyText + "");
 
@@ -82,3 +117,29 @@ function buildGifyCard(gifyImage, gifyText) {
     $("#view-area").append(viewItem);
 
 }
+
+function gifyImageClickHandler(){
+    var img = $(this)
+    var state = img.attr("data-img-state");
+    var stillImg = img.attr("data-img-still");
+    var activeImg = img.attr("data-img-active"); 
+
+
+    if (state === "still"){
+        img.attr("src", activeImg);
+        img.attr("data-img-state", "active");
+    } else {
+        img.attr("src", stillImg);
+        img.attr("data-img-state", "still");
+    }
+
+}
+
+
+function run() {
+    for (var i = 0; i < topics.length; i++) {
+        buildGifyButton(i, topics[i], topics[i]);
+    }
+}
+
+run();
--- assets/javascript/connections.js
+++ assets/javascript/connections.js
@@ -2,9 +2,9 @@ var key = "api_key=uOnMzSmMAIr9qzOMFZMaf9rIWawy7XDW";
 
 var queryURLTrending = "https://api.giphy.com/v1/gifs/trending?"+key;
 
-var queryURLRandom = "api.giphy.com/v1/stickers/random?"+key;
+var queryURLRandom = "https://api.giphy.com/v1/stickers/random?"+key;
 
-console.log("queryURLTrending: " + queryURLTrending);
+var queryURLSearch = "https://api.giphy.com/v1/gifs/search?"+key;
 
 
 
